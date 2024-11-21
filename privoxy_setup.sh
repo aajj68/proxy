db8cf9fc3a2ce421f4f7b456104052bde36058af
aajj68 <info@sgd.com.br>
2024-11-20 22:12:12 -0300

    fixing bugs

--- privoxy_setup.sh
+++ privoxy_setup.sh
@@ -15,11 +15,12 @@ update_external_ip() {
 EXTERNAL_IP=$(curl -s ifconfig.me)
 update_external_ip $EXTERNAL_IP
 
-#echo "Starting Privoxy..."
-#/usr/sbin/privoxy --no-daemon --pidfile "${PID_FILE}" "${CFG_FILE}" &
+echo "Starting Privoxy..."
+/usr/sbin/privoxy --no-daemon --pidfile "${PID_FILE}" "${CFG_FILE}" &
 
 # Loop para verificar mudanças no IP externo
 while true; do
+    sleep 300  # Verifica a cada 5 minutos
     NEW_IP=$(curl -s ifconfig.me)
     if [ "$NEW_IP" != "$EXTERNAL_IP" ]; then
         echo "External IP changed from $EXTERNAL_IP to $NEW_IP"
@@ -27,8 +28,9 @@ while true; do
         update_external_ip $EXTERNAL_IP
         # Reinicia o Privoxy para aplicar as mudanças
         echo "Restarting Privoxy..."
+        echo "Stopping Privoxy..."
         killall privoxy
-        exec /usr/sbin/privoxy --no-daemon --pidfile "${PID_FILE}" "${CFG_FILE}"
+        echo "Starting privoxy..."
+        /usr/sbin/privoxy --no-daemon --pidfile "${PID_FILE}" "${CFG_FILE}" &
     fi
-    sleep 300  # Verifica a cada 5 minutos
 done
