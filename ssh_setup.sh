d9dac5de335e3e93943e1f439f9ed5600e654d45
aajj68 <info@sgd.com.br>
2024-12-08 12:32:08 -0300

    Translate comments

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -5,23 +5,23 @@
 adduser -h /home/prvxy -s /bin/sh -D prvxy
 echo -n 'prvxy:prvxy' | chpasswd
 
-echo "Inicializando sshd service..."
+echo "Initializing SSHD Service..."
 ssh-keygen -A
 /usr/sbin/sshd -D -e "$@" &
 
 echo "Diretório de trabalho para chaves SSH"
 SSH_DIR="/home/prvxy/.ssh"
 
-echo "Criação do diretório para chaves SSH"
+echo "Creation of the SSH Keys Directory"
 mkdir -p $SSH_DIR
 chmod 700 $SSH_DIR
 
 if [ -f "/usr/local/bin/authorized_keys" ]; then
-    echo "Copiando o authorized_keys."
+    echo "Copying authorized_keys."
     mv /usr/local/bin/authorized_keys "${SSH_DIR}/authorized_keys"
 fi
 
-echo "Gera um par de chaves SSH sem senha"
+echo "Generates a pair of SSH keys without password"
 KEY_FILE="${SSH_DIR}/id_rsa"
 
 if [ ! -f "${KEY_FILE}" ]; then
@@ -35,9 +35,9 @@ fi
 
 chown -R prvxy:prvxy /home/prvxy/
 
-echo "Configura o túnel SSH para SOCKS5"
-REMOTE_HOST="localhost"  # Usando localhost para o proxy interno
+echo "Set the SSH Tunnel for Socks5"
+REMOTE_HOST="localhost"  # Using localhost for the internal proxy
 REMOTE_PORT=22
-SOCKS_PORT=5000  # Porta local para o proxy SOCKS5
+SOCKS_PORT=5000  # LOCAL DOOR FOR PROXY SOCKS5
 ssh -i "${KEY_FILE}" -o StrictHostKeyChecking=no -v -N -D 0.0.0.0:5000 -p 22 prvxy@localhost &
 echo "SOCKS5 proxy initialized on localhost:${SOCKS_PORT}"
